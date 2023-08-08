02e79feb36cb341a633a765fdbb9cff552c922e8
Souvik De <souvik.de@sato-global.com>
2023-08-08 14:09:26 +0900

    Feat:Hooking up Firebase Realtime Database to our Project.

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+const appSettings = {
+    databaseURL: "https://playground-7dac7-default-rtdb.asia-southeast1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const expenditureInDB = ref(database, "expense")
+
+console.log(app)
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
-    console.log(inputValue)
+    push(expenditureInDB,inputValue)
+    console.log(`${inputValue} added to database`)
 })
