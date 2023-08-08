193af438d1470b2611f0136d2fabc4c22323b721
Souvik De <souvik.de@sato-global.com>
2023-08-08 16:02:43 +0900

    Feat:Storing objects from the InputField to the Database and also display them on the page below.

--- script.js
+++ script.js
@@ -3,17 +3,24 @@ import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.
 const appSettings = {
     databaseURL: "https://playground-7dac7-default-rtdb.asia-southeast1.firebasedatabase.app/"
 }
-
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
 const expenditureInDB = ref(database, "expense")
-
-console.log(app)
+const expenseListEl= document.getElementById("expenditure-list")
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
     push(expenditureInDB,inputValue)
+    clearInputFieldEl()
+    appendItemToExpenseListEl(inputValue)
     console.log(`${inputValue} added to database`)
 })
+
+function clearInputFieldEl(){
+    inputFieldEl.value = ""
+}
+function appendItemToExpenseListEl(itemValue){
+    expenseListEl.innerHTML += `<li>${itemValue}</li>`
+}
